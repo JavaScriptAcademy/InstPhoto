ed90fe66ad5e863effe86359d869492cacb3fd4e
ElliotShi <elliotshi@outlook.com>
2016-04-13 17:06:40 +0800

    init project

---
+++ www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('shootCtrl', function($scope) {
+
+})
+   
+.controller('currentlyUserCtrl', function($scope) {
+
+})
+      
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('editPostCtrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,71 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.home', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.shoot', {
+    url: '/page3',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/shoot.html',
+        controller: 'shootCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.currentlyUser', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/currentlyUser.html',
+        controller: 'currentlyUserCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('signup', {
+    url: '/page5',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('login', {
+    url: '/page6',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('editPost', {
+    url: '/page7',
+    templateUrl: 'templates/editPost.html',
+    controller: 'editPostCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page1/page2')
+
+  
+
+});
